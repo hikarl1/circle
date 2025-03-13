eca8fdebd456fcca769e2d911aea7a397e3b2136
hikarl1 <83234547+hikarl1@users.noreply.github.com>
2025-03-13 13:09:51 +0700

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktic.geometry.shapes;
+
+/**
+ *
+ * @author Aurel
+ */
+/* */
+public class Circle {
+    private int id;
+    private double radius;
+    private final int pembilang = 22;
+    private final int penyebut = 7;
+    
+    public Circle(){}//konstruktor
+    public void setId(int id){ //method
+        this.id = id;
+    }
+    
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+    
+    public double getKeliling(){
+        return (pembilang/penyebut) * radius * 2;
+    }
+    
+    public double getLuas(){
+        return (pembilang/penyebut)* radius * radius ;
+    }
+    
+    public void printInfoCircle(){
+        System.out.println( 
+            "Id: " +id+ "\n"+
+            "Radius: " +radius+ "\n"+
+            "Keliling: "+ getKeliling()+"\n"+
+            "Luas: " +getLuas());
+    }
+}
+    
